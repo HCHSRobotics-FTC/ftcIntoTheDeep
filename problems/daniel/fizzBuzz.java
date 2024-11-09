c30b73950aacef12cb2245f0e75031bb7c58a202
PersoNameDaniel <dmolsson@outlook.com>
2024-11-09 14:35:28 -0600

    FizzBuzz completed

---
+++ problems/daniel/fizzBuzz.java
@@ -0,0 +1,19 @@
+public class fizzBuzz {
+    public static void main(String[] args) {
+        final int start = 0;
+        final int end = 100;
+
+        int current = start;
+
+        while (current <= (end)) {
+            if ((current % 3) == 0)
+                System.out.print("Fizz");
+            if ((current % 5) == 0)
+                System.out.print("Buzz");
+            if ((current % 3) != 0 && (current % 5) != 0)
+                System.out.print(current);
+            System.out.println();
+            current++;
+        }
+    }
+}
