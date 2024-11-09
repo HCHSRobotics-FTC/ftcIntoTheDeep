68f69efe79fb9e15fa44fa7c91e6a7de2b8d7dcb
PersoNameDaniel <dmolsson@outlook.com>
2024-11-09 14:35:40 -0600

    add bazz

--- problems/daniel/fizzBuzz.java
+++ problems/daniel/fizzBuzz.java
@@ -10,7 +10,9 @@ public class fizzBuzz {
                 System.out.print("Fizz");
             if ((current % 5) == 0)
                 System.out.print("Buzz");
-            if ((current % 3) != 0 && (current % 5) != 0)
+            if ((current % 7) == 0)
+                System.out.print("Bazz");
+            if ((current % 3) != 0 && (current % 5) != 0 && (current % 7) != 0)
                 System.out.print(current);
             System.out.println();
             current++;
