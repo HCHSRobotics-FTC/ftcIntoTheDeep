c13ad00cfc68ca10fff223260eb35b5935a92b10
PersoNameDaniel <dmolsson@outlook.com>
2024-12-07 14:51:50 -0600

    add alt stragegy

--- problems/daniel/trueCount/arrayTrue.java
+++ problems/daniel/trueCount/arrayTrue.java
@@ -5,14 +5,25 @@ public class arrayTrue {
         final boolean[] inputArray = {true, false, false, true, false, true, true, false, true, true, false, false, false};
 
         int trueCount = countTrue(inputArray);
+        int altTrueCount = altCountTrue(inputArray);
         
         System.out.printf("Number of trues in array: %d\n", trueCount);
+        System.out.printf("Number of trues in array (alt): %d\n", altTrueCount);
 
     }
     public static int countTrue(boolean[] input) {
         int trueCount = 0;
         for (boolean value : input) {
             if (value == true) {
+                trueCount++;
+            }
+        }
+        return trueCount;
+    }
+    public static int altCountTrue(boolean[] input) {
+        int trueCount = 0;
+        for (int i=0; i<input.length; i++) {
+            if (input[i] == true) {
                 trueCount++;
             }
         }
